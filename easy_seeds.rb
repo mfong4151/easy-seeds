92a1c0faab491f80dd505279dce408e9b81379c8
Max <mfong415@gmail.com>
2023-03-02 19:15:59 -0800

    Added error handling for photos, and extra puts statements for single seeder

--- easy_seeds.rb
+++ easy_seeds.rb
@@ -1,29 +1,72 @@
 require 'csv'
 
 
+=begin
+    Common preconditions:
+    1. table: Expects an array version of your table that corresponds to the name. 
+        Your table MUST be formated as an array containing multiple hashes, see the example from my menu_items class on github.
+        Because of how this is set up, the variable name MUST be equivalent to the name of the table in migrations.
+
+    2. class_name: Expects the actual class name created in models. Expects an actual class variable.
+
+    3. table_string: Table name given as a string. Usually used for destroying tables ahead of import. Expects a string
+
+    Common Issues:
+
+    Deleting: The issue occurs if theres a directed graph cycle relation between tables, or if there are joins tables with multiple presence constraints on the foreign keys. 
+    Tip: To avoid this you have to typically drop the entire database, although this is obviously not recommended in production.
+
+=end
+
 class EasySeeds
 
     #Creates a single instance of seed data
+
     def self.single_seeder(table, class_name, table_string)
-        class_name.destroy_all
+
         ApplicationRecord.connection.reset_pk_sequence!(table_string)
         puts "Creating #{table_string} seed data..."   
-        table.each {|row| class_name.create!(**row)}
+        
+        table.each_with_index do |row, i| 
+          puts "Finished Seeding the #{i.to_s}th #{table_string} item" if i % 100 >= 1
+          class_name.create!(**row)
+
+        end
+            
         puts "DONE WITH #{table_string.upcase}, #{table_string.upcase} SEEDING SUCCESSFUL"
         
     end 
 
     #Creates easy seed data for all classes that are passed in
+
     def self.create_easy_seed_data(class_names)
+
       tables, table_strings = csv_to_seeds = EasySeeds.tables_from_csvs
       (0...tables.length).each{|i| EasySeeds.single_seeder(tables[i], class_names[i], table_strings[i])}
     
     end
 
+
+
+    def self.find_photo_folder
+      seed_folder =''
+      cwd = Dir.pwd
+
+      if cwd.include('/db')
+      end
+
+    end
+
+
     #Attaches images
+
     def self.attach_images(class_image_names)
-      seed_folder = '../seed_image_files'
-      Dir.chdir(seed_folder)
+
+      begin
+        Dir.chdir('../seed_image_files')
+      rescue
+        Dir.chdir(Dir.pwd + '/db/seed_image_files')
+      end
 
       Dir.glob("*").each_with_index do |seed_file, i|
         headers, data = EasySeeds.unpack_csvs(seed_file)
@@ -31,12 +74,20 @@ class EasySeeds
         puts "Attaching to #{class_image_name}..."
 
         data.each_with_index do |row|
+
             object_id, url, filename = row
             class_instance = class_image_name.find_by_id(object_id)
-            puts class_instance, url, filename
-            class_instance.image.attach(io: URI.open(url), filename: filename)
-            puts "Attached to #{filename}"
 
+            begin
+              class_instance.image.attach(io: URI.open(url), filename: filename)
+              puts "Attached to #{filename}"
+
+            rescue OpenURI::HTTPError
+              puts('Waiting 30 seconds before seeding the next row of data, please be patient')
+              sleep(30.second)
+              class_instance.image.attach(io: URI.open(url), filename: filename)
+              puts "Attached to #{filename}"
+            end
         end
       end
     end
@@ -52,10 +103,13 @@ class EasySeeds
 
     ###Used in conjunction to destroy all of your tables
     def self.destroy_tables(class_names, table_strings)
-      (class_names.length - 1).downto(0) {|i| EasySeeds.destroy_table(class_names[i], table_strings[i])}
+
+      (class_names.length - 1).downto(0) {|i|EasySeeds.destroy_table(class_names[i], table_strings[i])}
     end
 
+
     ##Reads and parses CSVS
+
     def self.unpack_csvs(seed_file)
       data = []
       CSV.foreach(seed_file) {|row|data << row}
@@ -72,29 +126,31 @@ class EasySeeds
         all_seed_data = []
         table_strings = []
         seed_folder = './db/seed_files'
+        
         Dir.chdir(seed_folder)
         
         Dir.glob("*").each do |seed_file|
-        
+
             seed_res = []
             headers, data = unpack_csvs(seed_file)
             
             data.each_with_index do |row, j|
+                
                 datum = {}
-
+            
                 row.each_with_index do |col, i|
+                    
                   key = EasySeeds.clean_headers(headers[i])
                   datum[key[0]] = type_conversion(row[i], key[1])
                 
                 end
             
                 seed_res << datum
-
             end
+            
             
             table_strings << seed_file[seed_file.index('_') + 1..-11]
             all_seed_data << seed_res
-            
         end
         
         return all_seed_data, table_strings
@@ -104,16 +160,12 @@ class EasySeeds
     protected
 
     def self.clean_headers(header)
-
         if header.include?(":")
           first, second, = header.split(":")
           header_and_type = [first.downcase, second]
-
         else
           header_and_type = [header.downcase, 'string']
-
         end
-
         return header_and_type
     end
 
@@ -132,9 +184,11 @@ class EasySeeds
         return datum.to_i
       
       elsif ['float', 'f'].include?(data_type)
+      
         return datum.to_f
 
       elsif ["bool", "boolean"].include?(data_type)
+
         return ActiveModel::Type::Boolean.new.cast(datum)
 
       elsif ["date"].include?(data_type)
@@ -142,10 +196,6 @@ class EasySeeds
 
       else
         return datum
-
       end
     end
 end
-
-
-      
