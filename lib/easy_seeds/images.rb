5d284c520080b71b4d983e124269370255d183a6
Max <mfong415@gmail.com>
2023-04-04 11:22:27 -0700

    Refacotred seeder

--- lib/easy_seeds/images.rb
+++ lib/easy_seeds/images.rb
@@ -14,26 +14,26 @@ module EasySeeds
             end
   
             Dir.glob("*").each_with_index do |seed_file, i|
-          headers, data = EasySeeds.unpack_csvs(seed_file)
-          class_image_name = class_image_names[i]
-          puts "Attaching to #{class_image_name}..."
+              headers, data = EasySeeds.unpack_csvs(seed_file)
+              class_image_name = class_image_names[i]
+              puts "Attaching to #{class_image_name}..."
   
-          data.each_with_index do |row|
+              data.each_with_index do |row|
   
-              object_id, url, filename = row
-              class_instance = class_image_name.find_by_id(object_id)
+                object_id, url, filename = row
+                class_instance = class_image_name.find_by_id(object_id)
   
-              begin
-                class_instance.image.attach(io: URI.open(url), filename: filename)
-                puts "Attached to #{filename}"
+                begin
+                  class_instance.image.attach(io: URI.open(url), filename: filename)
+                  puts "Attached to #{filename}"
   
-              rescue OpenURI::HTTPError
-                puts('Waiting 30 seconds before seeding the next row of data, please be patient')
-                sleep(30.second)
-                class_instance.image.attach(io: URI.open(url), filename: filename)
-                puts "Attached to #{filename}"
+                rescue OpenURI::HTTPError
+                  puts('Waiting 30 seconds before seeding the next row of data, please be patient')
+                  sleep(30.second)
+                  class_instance.image.attach(io: URI.open(url), filename: filename)
+                  puts "Attached to #{filename}"
+                end
               end
-          end
             end
         
 
