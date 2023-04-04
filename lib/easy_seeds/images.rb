c020e554536e2f621e68c1ddfff3c9356026bfee
Max <mfong415@gmail.com>
2023-04-04 11:07:37 -0700

    Broke up classess for easy of editing

---
+++ lib/easy_seeds/images.rb
@@ -0,0 +1,40 @@
+module EasySeeds
+    class Images
+         #Attaches images
+
+        def self.attach_images(class_image_names)
+
+            begin
+          Dir.chdir('../seed_image_files')
+            rescue
+          Dir.chdir(Dir.pwd + '/db/seed_image_files')
+            end
+  
+            Dir.glob("*").each_with_index do |seed_file, i|
+          headers, data = EasySeeds.unpack_csvs(seed_file)
+          class_image_name = class_image_names[i]
+          puts "Attaching to #{class_image_name}..."
+  
+          data.each_with_index do |row|
+  
+              object_id, url, filename = row
+              class_instance = class_image_name.find_by_id(object_id)
+  
+              begin
+                class_instance.image.attach(io: URI.open(url), filename: filename)
+                puts "Attached to #{filename}"
+  
+              rescue OpenURI::HTTPError
+                puts('Waiting 30 seconds before seeding the next row of data, please be patient')
+                sleep(30.second)
+                class_instance.image.attach(io: URI.open(url), filename: filename)
+                puts "Attached to #{filename}"
+              end
+          end
+            end
+        
+
+        end
+    end
+
+end
---
+++ lib/easy_seeds/seeder.rb
@@ -0,0 +1,33 @@
+require_relative 'csv_methods'
+
+module EasySeeds 
+    class Seeder
+        include CSVS
+        #Creates a single instance of seed data
+
+        def self.single_seeder(table, class_name, table_string)
+
+            ApplicationRecord.connection.reset_pk_sequence!(table_string)
+            puts "Creating #{table_string} seed data..."   
+
+            table.each_with_index do |row, i| 
+              puts "Finished Seeding the #{i.to_s}th #{table_string} item" if i % 100 == 0
+              class_name.create!(**row)
+
+            end
+
+            puts "DONE WITH #{table_string.upcase}, #{table_string.upcase} SEEDING SUCCESSFUL"
+
+        end 
+
+        #Creates easy seed data for all classes that are passed in
+
+        def self.create_easy_seed_data(class_names)
+
+          tables, table_strings = csv_to_seeds = EasySeeds.tables_from_csvs
+          (0...tables.length).each{|i| EasySeeds.single_seeder(tables[i], class_names[i], table_strings[i])}
+        
+        end
+
+    end
+end
