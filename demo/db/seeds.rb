c95f1bca23f83c393213e72d9e210de1943acf70
Max <mfong415@gmail.com>
2023-04-10 10:39:41 -0700

    Refactored demo

--- demo/db/seeds.rb
+++ demo/db/seeds.rb
@@ -1,16 +1,16 @@
-require 'easy_seeds/easy_seeds'
+require 'easy_seeds/easy_seeds' #Step 5.
 
 
 ##Initialize tables in order we want things to be seeded. There is a foreign key on Menus pointing to Restaurant, so we seed Restaurant first
 ##In other words, things should be seeded in the topologically sorted order.
 
-class_names = [User, Restaurant, Menu, MenuItem, Review, Cart, CartItem, Transaction, Location]
+class_names = [User, Restaurant, Menu, MenuItem, Review, Cart, CartItem, Transaction, Location]  #Step 6.
 table_strings = ['users', 'restaurants', 'menus', 'menu_items', 'reviews', 'carts', 'cart_items', 'transactions', 'locations']
 
-EasySeeds::Destroy.destroy_tables(class_names, table_strings)
+EasySeeds::Destroy.destroy_tables(class_names, table_strings)  #Optional: erases data in your tables. Sometimes it might be easier to just drop tables altogether
 
-EasySeeds::Seeder.create_easy_seed_data(class_names)
+EasySeeds::Seeder.create_easy_seed_data(class_names)  #Step 7. 
 
 
-class_image_names = [Restaurant]
+class_image_names = [Restaurant]  #An example of seeding photos, please note that the demo files belong to a protected AWS bucket and will not work
 EasySeeds::Images.attach_images(class_image_names)
